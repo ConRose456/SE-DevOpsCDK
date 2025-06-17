8befa859d3f3a41e8ef6e1f03110f144473350f3
Connor Rosewell <rosewcon@amazon.com>
2025-06-17 22:42:26 +0100

    Adding GraphQl API lambda and api gateway

--- src/serviceStack.ts
+++ src/serviceStack.ts
@@ -1,4 +1,6 @@
 import { Duration, RemovalPolicy, StackProps, Stack } from "aws-cdk-lib";
+import { CorsHttpMethod, HttpApi, HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import {
   CachePolicy,
   Distribution,
@@ -6,9 +8,11 @@ import {
   ViewerProtocolPolicy,
 } from "aws-cdk-lib/aws-cloudfront";
 import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import * as path from "path";
 
 interface ServiceStackProps extends Omit<StackProps, "softwareType"> {
   stage: string;
@@ -36,11 +40,33 @@ export class DevOpsCdkStack extends Stack {
       },
     );
 
-    createCloudFrontDist({
+    const distribution = createCloudFrontDist({
       scope: this,
       bucket: webAssetsBucket,
       defaultPage,
       props,
+    });
+
+    const graphqlLambda = new Function(this, `${props.stage}-GraphQlLambda`, {
+        runtime: Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        code: Code.fromAsset(
+            path.resolve(__dirname, '../node_modules/@con-rose/devopsservice/dist')
+        )
+    });
+
+    const httpApi = new HttpApi(this, `${props.stage}-GraphQlApiGateway`, {
+        apiName: `${props.stage}-GraphQlService`,
+        corsPreflight: {
+            allowOrigins: [distribution.domainName],
+            allowMethods: [CorsHttpMethod.POST]
+        }
+    });
+
+    httpApi.addRoutes({
+        path: "/graphql",
+        methods: [HttpMethod.POST],
+        integration: new HttpLambdaIntegration(`${props.stage}-LambdaIntegration`, graphqlLambda)
     });
   }
 }
@@ -96,4 +122,6 @@ const createCloudFrontDist = ({
     },
   );
   distribution.node.addDependency(defaultPage);
+
+  return distribution;
 };
